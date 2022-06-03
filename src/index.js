1f18de4da4ed43eb39308afdeba1a54d60a0fcba
Muralidhar <muralidhar7794@gmail.com>
2022-06-03 20:02:46 +0530

    fix: revamp of whole site

--- src/index.js
+++ src/index.js
@@ -1,17 +1,41 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+async function displayProjects(){
+    const projectsContainer = document.querySelector(".projects-container");
+    const projectsList = await fetchData();
+    const neogCamp = projectsList.neogcampProjects;
+    
+    neogCamp.forEach(element => {
+        const article = document.createElement("article");
+        const source = document.createElement("a");
+        const live = document.createElement("a");
+        const title = document.createElement("h2");
+        const liveText = document.createTextNode("Live");
+        const sourceText = document.createTextNode("Source");
+         
+        
+        if(element.id % 2 !== 0){
+            article.className = "project-wrapper odd-bg"
+        }
+        else article.className = "project-wrapper"
+        
+        live.className = "link link-primary"
+        source.className = "link link-secondary"
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+        source.setAttribute("href", `${element.source}`);
+        source.appendChild(sourceText)
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+        live.setAttribute("href",`${element.url}`)
+        live.appendChild(liveText)
+
+        const textNode = document.createTextNode(`${element.name}`);
+        title.appendChild(textNode)
+        article.append(title,live,source)
+        projectsContainer.appendChild(article)
+    });
+}
+
+async function fetchData(){
+    const response = await fetch("./data.json");
+    const data = await response.json();
+    return data;
+}
+displayProjects();
