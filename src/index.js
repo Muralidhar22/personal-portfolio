6a8262c4724888e67d91bbb3bec30fdbaf15ac76
Muralidhar <muralidhar7794@gmail.com>
2022-06-05 22:38:40 +0530

    fix: theme change, blogs added, project added

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
+// display projects
 async function displayProjects(){
+    const goToTop = document.getElementById("goToTop")
     const projectsContainer = document.querySelector(".projects-container");
-    const projectsList = await fetchData();
-    const neogCamp = projectsList.neogcampProjects;
+    const data = await fetchData();
+    const neogCamp = data.neogcampProjects;
     
     neogCamp.forEach(element => {
         const article = document.createElement("article");
@@ -10,8 +12,10 @@ async function displayProjects(){
         const title = document.createElement("h2");
         const liveText = document.createTextNode("Live");
         const sourceText = document.createTextNode("Source");
-         
+        const date= document.createTextNode(`${element.date}`)
+        const smallElement = document.createElement("small")
         
+        // setting class names
         if(element.id % 2 !== 0){
             article.className = "project-wrapper odd-bg"
         }
@@ -19,6 +23,12 @@ async function displayProjects(){
         
         live.className = "link link-primary"
         source.className = "link link-secondary"
+        smallElement.className = "date block"
+
+        // setting attributes and appending child elements
+        source.setAttribute("target","blank")
+        live.setAttribute("target","blank")
+        
 
         source.setAttribute("href", `${element.source}`);
         source.appendChild(sourceText)
@@ -28,9 +38,29 @@ async function displayProjects(){
 
         const textNode = document.createTextNode(`${element.name}`);
         title.appendChild(textNode)
-        article.append(title,live,source)
+        smallElement.appendChild(date)
+        article.append(title,smallElement,live,source)
         projectsContainer.appendChild(article)
     });
+
+    // go to top of the page
+    goToTop.addEventListener('click',topFunction)
+    // When the user scrolls down 20px from the top of the document, show the button
+    window.onscroll = function() {scrollFunction()};
+
+    function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        goToTop.style.display = "block";
+    } else {
+        goToTop.style.display = "none";
+    }
+    }
+
+    // When the user clicks on the button, scroll to the top of the document
+    function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+    }
 }
 
 async function fetchData(){
@@ -38,4 +68,5 @@ async function fetchData(){
     const data = await response.json();
     return data;
 }
+
 displayProjects();
